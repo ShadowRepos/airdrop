be33a2cce546a437200b11c96258c5a7b825b6b5
ShadowRepos <132324874+ShadowRepos@users.noreply.github.com>
2023-05-03 18:20:14 +0900

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -36,23 +36,19 @@ function mvContract() {
   return new web3js.eth.Contract(erc20_abi, mv_addr);
 }
 
-const land_env_key = "development";
+const land_env_key = "production";
 const land_env = {
   development: {
-    mv_addr: "0xEAd0DCAdD8A7E6F9FC61F0F419C3005B80683378", // MVのトークンアドレステスト用
-    spender_addr: "0x0F33BFeF7cf01AE494F72ea01C0C47fdBa4Db672", // AWS上のウォレットアドレス
-    network: "mumbai",
-    land_addr: "0x9F113275624153ca030525f96C840D0621129C53",
+    mv_addr: "0xEAd0DCAdD8A7E6F9FC61F0F419C3005B80683378",
+    spender_addr: "0x861d0636b02B239b7a8f5bEF39F305cbF938cBd4",
     base_url: "http://localhost:50080/",
-    net_id: 80001, // polygon testnet のnetwork ID
+    net_id: 80001,
   },
   production: {
-    mv_addr: "0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E", // MVのトークンアドレス
-    spender_addr: "0x0F33BFeF7cf01AE494F72ea01C0C47fdBa4Db672", // AWS上のウォレットアドレス
-    network: "matic",
-    land_addr: "0x5cbc94a38b793EF1756BF6a0CdA91A1382b60aE6",
-    base_url: "https://api01.genso.game/",
-    net_id: 137, // polygon のnetwork ID
+    mv_addr: "0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E",
+    spender_addr: "0x861d0636b02B239b7a8f5bEF39F305cbF938cBd4",
+    base_url: "",
+    net_id: 1,
   },
 };
 
